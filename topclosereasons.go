166626a1fba78c04124cb4cb4581faea1b87226f
Ben <ben@ticketsbot.cloud>
2025-06-01 10:27:51 +0100

    feat: GetTopCloseReasonsContaining
    Signed-off-by: Ben <ben@ticketsbot.cloud>

--- topclosereasons.go
+++ topclosereasons.go
@@ -48,7 +48,37 @@ LIMIT 10`
 	}
 
 	defer rows.Close()
-	
+
+	reasons := make([]string, 10)
+	i := 0
+	for rows.Next() {
+		var reason string
+		if err := rows.Scan(&reason); err != nil {
+			return nil, err
+		}
+
+		reasons[i] = reason
+		i++
+	}
+
+	return reasons[:i], nil
+}
+
+func (c *Client) GetTopCloseReasonsContaining(context context.Context, guildId uint64, panelId *int, contains string) ([]string, error) {
+	query := `
+SELECT close_reason
+FROM analytics.top_close_reasons
+WHERE guild_id = ? AND panel_id = ? AND LOWER(close_reason) '%' || LIKE LOWER(?) || '%'
+ORDER BY ranking ASC
+LIMIT 10`
+
+	rows, err := c.client.Query(context, query, guildId, panelId, contains)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
 	reasons := make([]string, 10)
 	i := 0
 	for rows.Next() {
